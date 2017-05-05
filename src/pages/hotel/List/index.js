1098101614bab5f3396a517e893fec6f658f72fe
pengxianghai(彭祥海) <xianghai.peng@jv.jb-aptech.com.cn>
2017-05-05 17:42:51 +0800

    initial commit

--- src/pages/hotel/List/index.js
+++ src/pages/hotel/List/index.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import Header from '../../../components/Header'
-import Footer from '../../../components/Footer'
 import Home from '../../Home'
 
 export default class App extends React.Component {
   render () {
     return (
       <div>
-        <Header />
         <Home />
-        <Footer />
+        <div>
+				ceshixinx
+        </div>
+
+        <div>
+				ceshixinx
+        </div>
+
+        <div>
+				ceshixinx
+        </div>
       </div>
     )
   }
