bf3ac1cf9f2599a574533284cd38f906f7e9d062
pengxianghai(彭祥海) <xianghai.peng@jv.jb-aptech.com.cn>
2017-05-06 16:40:50 +0800

    listOption commit

--- src/pages/HotelList/index.js
+++ src/pages/HotelList/index.js
@@ -1,3 +1,102 @@
 import React from 'react'
 
-export default () => <h1>HotelList13434343</h1>
+//列表页选项组件
+export default class ListOption extends React.Component {
+	render() {
+		return (
+	<div className="travel tavern-list">
+        <div className="tavern-list-head">
+          <div className="localtion-item">您所在的位置：<a>酒店预订&gt;</a>北京酒店</div>
+        </div>
+        <div className="travel-body list-body">
+          <div id="myTabContent" className="tavern-list-param">
+            <div className="search-title">
+              目的地：<input id="tavernCitySelect1" defaultValue="北京市" placeholder="请选择目的地" />
+              入住： <input type="text" placeholder="2016-12-08" onFocus="WdatePicker({doubleCalendar:true,dateFmt:'yyyy-MM-dd',position:{left:0,top:0}})" />
+              退房：<input type="text" placeholder="2016-12-20" onFocus="WdatePicker({doubleCalendar:true,dateFmt:'yyyy-MM-dd',position:{left:0,top:0}})" />
+              关键词：<input />
+              <button className="btn btn-default" style={{fontSize: 14}}>搜索</button>
+            </div>
+            <div className="tavern-search-params">
+              <div className="search-one-param">
+                <span className="param-label">位置：</span>
+                <div className="checkbox-list">
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="locationCheckbox1" defaultValue="option1" defaultChecked />天安门、王...
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="locationCheckbox2" defaultValue="option2" /> 中关村、五...
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="locationCheckbox3" defaultValue="option3" />西单、金融...
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="locationCheckbox4" defaultValue="option3" />首都机场
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="locationCheckbox5" defaultValue="option3" />亚运村
+                  </label>
+                </div>
+              </div>
+              <div className="search-one-param">
+                <span className="param-label">价格：</span>
+                <div className="checkbox-list">
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="unitCheckbox1" defaultValue="option1" />¥150以下
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="unitCheckbox2" defaultValue="option2" defaultChecked />¥150-300
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="unitCheckbox3" defaultValue="option3" />¥301-450
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="unitCheckbox4" defaultValue="option3" />¥450以上
+                  </label>
+                </div>
+              </div>
+              <div className="search-one-param">
+                <span className="param-label">星级：</span>
+                <div className="checkbox-list">
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="starCheckbox1" defaultValue="option1" />二星级及以...
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="starCheckbox2" defaultValue="option2" defaultChecked />三星级/舒适
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="starCheckbox3" defaultValue="option3" />四星级/高档
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="starCheckbox4" defaultValue="option3" /> 五星级/高档
+                  </label>
+                </div>
+              </div>
+              <div className="search-one-param">
+                <span className="param-label">特色：</span>
+                <div className="checkbox-list">
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="featureCheckbox1" defaultValue="option1" />休闲度假
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="featureCheckbox2" defaultValue="option2" defaultChecked />青年旅社
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="featureCheckbox3" defaultValue="option3" />精品酒店
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="featureCheckbox4" defaultValue="option3" />商务出行
+                  </label>
+                  <label className="checkbox-inline">
+                    <input type="checkbox" id="featureCheckbox5" defaultValue="option3" />会
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+			)
+	}
+}
