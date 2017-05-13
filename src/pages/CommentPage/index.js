49e2f40c185cd64a2b888140cee236d73c86257e
pengxianghai(彭祥海) <xianghai.peng@jv.jb-aptech.com.cn>
2017-05-13 16:41:04 +0800

    tj

--- src/pages/CommentPage/index.js
+++ src/pages/CommentPage/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Icon, Layout, Input, Checkbox, Radio, Form, DatePicker, Row, Col, Button } from 'antd'
 const { Sider, Content } = Layout
+import Commentimg from './images/comment_img180180.jpg'
 
 /*import OrderDetailTab from 'components/OrderDetail/OrderDetailTab'
 */import './style.css'
@@ -11,37 +12,39 @@ export default class CommentPage extends React.Component {
         return (
             <div className="commentpage">
             {/*酒店圖片*/}
-              <Layout style={{ height: 240, textAlign: 'center', borderBottom: 0, marginBottom:20 }}>
-                <Sider style={{ background: '#fff', padding: 20, }}>订单信息</Sider>
-                <Content style={{ background: '#f2fcff' }}>
+              <Layout style={{ height: 240, textAlign: 'center', marginBottom:20 }}>
+                <Sider style={{ background: '#fff', padding: 20 }}>
+                  <img src={Commentimg} alt=""/>
+                </Sider>
+                <Content style={{padding:'25px' }}>
                   
                 </Content>
               </Layout>
             {/*总体评价*/}
-              <Layout style={{ height: 240, textAlign: 'center', borderBottom: 0, marginBottom:10 }}>
-                <Sider style={{ background: '#fff', padding: 20, }}>总体评价</Sider>
-                <Content style={{ background: '#f2fcff' }}>
+              <Layout style={{ height: 240, textAlign: 'center', marginBottom:10 }}>
+                <Sider style={{background: '#fff', padding: 35,borderRight:'2px dashed #ccc' }}>总体评价</Sider>
+                <Content style={{padding:'25px' }}>
                   
                 </Content>
               </Layout>
             {/*点评内容*/}
-              <Layout style={{ height: 240, textAlign: 'center', borderBottom: 0, marginBottom:10 }}>
-                <Sider style={{ background: '#fff', padding: 20, }}>点评内容</Sider>
-                <Content style={{ background: '#f2fcff' }}>
+              <Layout style={{ height: 240, textAlign: 'center', marginBottom:10 }}>
+                <Sider style={{ background: '#fff', padding: 35,borderRight:'2px dashed #ccc' }}>点评内容</Sider>
+                <Content style={{padding:'25px' }}>
                   
                 </Content>
               </Layout>
             {/*上传图片*/}
-              <Layout style={{ height: 240, textAlign: 'center', borderBottom: 0, marginBottom:10 }}>
-                <Sider style={{ background: '#fff', padding: 20, }}>上传图片（选填）</Sider>
-                <Content style={{ background: '#f2fcff' }}>
+              <Layout style={{ height: 240, textAlign: 'center', marginBottom:10 }}>
+                <Sider style={{ background: '#fff', padding: 35,borderRight:'2px dashed #ccc' }}>上传图片（选填）</Sider>
+                <Content style={{padding:'25px' }}>
                   
                 </Content>
               </Layout>
             {/*优点不足（选填）*/}
-              <Layout style={{ height: 240, textAlign: 'center', borderBottom: 0, marginBottom:30 }}>
-                <Sider style={{ background: '#fff', padding: 20, }}>优点不足（选填）</Sider>
-                <Content style={{ background: '#f2fcff' }}>
+              <Layout style={{ height: 240, textAlign: 'center', marginBottom:30 }}>
+                <Sider style={{ background: '#fff', padding: 35,borderRight:'2px dashed #ccc' }}>优点不足（选填）</Sider>
+                <Content style={{padding:'25px' }}>
                   
                 </Content>
               </Layout>
