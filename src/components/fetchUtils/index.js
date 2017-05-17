531247e9d654843cb3158be525ec1904393f73f2
zh zbin <zh zbin>
2017-05-17 15:47:45 +0800

    请求加载方式

---
+++ src/components/fetchUtils/index.js
@@ -0,0 +1,41 @@
+
+
+import React from 'react'
+let fetchRequest = function (params) {
+
+  fetch(params.url, {
+    method: params.type ? params.type : "GET",
+    mode: "cors",
+    dataType: "jsonp",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: params.param
+  })
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(res => {
+            params.callback(res);
+          })
+      }
+    })
+
+
+}
+export default fetchRequest;
+let fetchBiz = function (params) {
+  // /biz/api
+  params.url = "/biz/api" + params.url
+  fetchRequest(params);
+}
+let fetchAuth = function (params) {
+  // auth/api
+  params.url = "/biz/api" + params.url
+  fetchRequest(params);
+}
+let fetchSearch = function (params) {
+  // /search/api/
+  params.url = "/biz/api" + params.url
+  fetchRequest(params);
+}
+export { fetchAuth, fetchBiz, fetchSearch };
--- src/pages/HotelDetail/video.js
+++ src/pages/HotelDetail/video.js
@@ -4,75 +4,87 @@
 * @Last Modified by:   zhzbin
 * @Last Modified time: 2017-05-08 13:37:00
 */
-import React from 'react' 
+import React from 'react'
 
 import video_01 from '../../common/images/video/travel/video.mp4'
 import video_02 from '../../common/images/video/travel/video.webm'
 import video_03 from '../../common/images/video/travel/video1.mp4'
-import fetchRequest from './fetchHttpRequest.js'
+import { fetchBiz } from '../../components/fetchUtils'
 export default class Video extends React.Component {
-  render () {
+  render() {
     return (
       <div>
-        <VideoModel url=""/>
+        <VideoModel url="" />
         <VideoHeader />
       </div>
     )
   }
 }
-let imgSrc="";
+let imgSrc = "";
 /* 视频显示组件， 用于加载视频头部数据信息*/
 
- export class VideoHeader extends React.Component {
-    constructor(props) {
-        super(props);
+export class VideoHeader extends React.Component {
+  constructor(props) {
+    super(props);
 
-        // 设置 initial state
-        this.state = {
-            text: props.initialValue || 'placeholder'
-        };
+    // 设置 initial state
+    this.state = {
+      text: props.initialValue || 'placeholder'
+    };
 
-        // ES6 类中函数必须手动绑定
-        // this.handleClick = this.handleClick.bind(this);
-      imgSrc=video_03;
-        
-    } 
-    componentWillMount () {
-    
-      fetchRequest("/biz/api/hotel/queryhotelfeature",{
-  "ascSort": "string",
-  "checkInDate": "2017-05-17T01:52:33.188Z",
-  "checkOutDate": "2017-05-17T01:52:33.188Z",
-  "descSort": "string",
-  "destination": "string",
-  "featureIds": "string",
-  "hotelLevel": 0,
-  "keywords": "string",
-  "maxPrice": 0,
-  "minPrice": 0,
-  "pageNo": 0,
-  "pageSize": 0,
-  "tradeAreaIds": "string"
-},function(e){
-        console.debug(e);
-      })
-       this.setState({
-                     title: "三亚喜来登度假酒店",
-                     desc: "Sheraton Sanya Resort",
-                     subDesc: "【 亚龙湾 】国家旅游度假区，亚龙湾高尔夫球场对面",
-                     descMap:["东南亚风情","休闲度假","亲子时刻","会议酒店"],
-                     video_01:video_01,
-                     video_02:video_02
-                })
-    }
+    // ES6 类中函数必须手动绑定
+    // this.handleClick = this.handleClick.bind(this);
+    imgSrc = video_03;
 
-   handleClick(event) {
-       document.getElementsByClassName("travel-model")[0].style["display"]="block";
-       let vm=document.getElementById("videoMessage");
-       vm.play();
-      // alert(" ceshi dhushu ")
-    } 
+  }
+  componentWillMount() {
+      var sef= this ;
+    fetchBiz({
+      url: "/hotel/queryhotelfeature",
+      param: {
+        "ascSort": "string",
+        "checkInDate": "2017-05-17T01:52:33.188Z",
+        "checkOutDate": "2017-05-17T01:52:33.188Z",
+        "descSort": "string",
+        "destination": "string",
+        "featureIds": "string",
+        "hotelLevel": 0,
+        "keywords": "string",
+        "maxPrice": 0,
+        "minPrice": 0,
+        "pageNo": 0,
+        "pageSize": 0,
+        "tradeAreaIds": "string"
+      },
+      type: "",
+      callback: function (e) {
+        sef.setState({
+          title: "三亚喜来登度假酒店",
+          desc: "Sheraton Sanya Resort",
+          subDesc: "【 亚龙湾 】国家旅游度假区，亚龙湾高尔夫球场对面",
+          descMap: ["东南亚风情", "休闲度假", "亲子时刻", "会议酒店"],
+          video_01: video_01,
+          video_02: video_02
+        })
+
+      }
+    })
+
+
+
+
+  }
+
+  handleClick(event) {
+    document.getElementsByClassName("travel-model")[0].style["display"] = "block";
+    let vm = document.getElementById("videoMessage");
+    vm.play();
+    // alert(" ceshi dhushu ")
+  }
   render() {
+    if(!this.state.descMap){
+      return <div></div>;
+    }
     return (
       <div className="i-tv">
         <div className="video-plugin ">
@@ -83,16 +95,16 @@ let imgSrc="";
               <div className="mes-button">{this.state.subDesc}
               </div>
               <div className="mes-button">
-               {this.state.descMap.map(function(item,index,arry){
-                 return  " ● "+ item+"    " ;
-               })}
+                {this.state.descMap.map(function (item, index, arry) {
+                  return " ● " + item + "    ";
+                })}
               </div>
               <div className="mes-play" ><i className=" icon-play-circle" onClick={this.handleClick} /><span>查看完整视频</span></div>
             </div>
           </div>
           <video className="video" autoPlay="autoplay" preload="auto" loop="loop">
-            <source src={this.state.video_02} type="video/webm" alt=""/>
-            <source src={this.state.video_01} type="video/mp4"  alt=""/>
+            <source src={this.state.video_02} type="video/webm" alt="" />
+            <source src={this.state.video_01} type="video/mp4" alt="" />
           </video>
         </div>
       </div>
@@ -102,31 +114,31 @@ let imgSrc="";
 }
 
 /* 用于展示弹出的数据窗口信息*/
-export class VideoModel extends React.Component{
-    state = {
-        source: ""
-    }
-    componentWillMount () {
-       this.setState({
-                    source: imgSrc
-                })
-        // const map = 'map'
-        // fetch('http://rapapi.org/mockjsdata/18171/api/todo/list')
-        //     .then(function(response) {
-        //         return response.json()
-        //     })
-        //     .then(res => {
-        //         this.setState({
-        //             source: imgSrc
-        //         })
-        //     })
-    }
-  hideWindow (e){
-    this.refs.videoModel.style["display"]="none"; 
-    this.refs.vm.pause();
-    this.refs.vm.currentTime=0;
+export class VideoModel extends React.Component {
+  state = {
+    source: ""
   }
-  render(){
+  componentWillMount() {
+    this.setState({
+      source: imgSrc
+    })
+    // const map = 'map'
+    // fetch('http://rapapi.org/mockjsdata/18171/api/todo/list')
+    //     .then(function(response) {
+    //         return response.json()
+    //     })
+    //     .then(res => {
+    //         this.setState({
+    //             source: imgSrc
+    //         })
+    //     })
+  }
+  hideWindow(e) {
+    this.refs.videoModel.style["display"] = "none";
+    this.refs.vm.pause();
+    this.refs.vm.currentTime = 0;
+  }
+  render() {
     return (
       <div className="travel-model" ref="videoModel">
         <div className="bg" />
@@ -138,13 +150,13 @@ export class VideoModel extends React.Component{
             </a>
           </div>
           <div className="model-body">
-            <video ref="vm" id="videoMessage" className="video-js vjs-default-skin" controls="false" loop preload="auto" 
-            width={710} height={401} src={video_03}  > 
-               <source src={this.state.source} type="video/mp4" />
+            <video ref="vm" id="videoMessage" className="video-js vjs-default-skin" controls="false" loop preload="auto"
+              width={710} height={401} src={video_03}  >
+              <source src={this.state.source} type="video/mp4" />
             </video>
           </div>
         </div>
       </div>
-      )
+    )
   }
 }
