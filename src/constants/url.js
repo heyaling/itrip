833cb959694101914f320e18b1890993a9a1f684
heyaling <kxxw168@163.com>
2017-05-19 12:59:44 +0800

    fix url

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,5 +1,5 @@
 // 获取城市对应酒店列表
-export const hotelListUrl = '/search/api/hotellist/searchItrpHotelListByHotCity'
+export const hotelListUrl = '/search/api/hotellist/searchItripHotelListByHotCity'
 
 // 获取国内热门酒店城市
 export const hotelCityUrl = '/biz/api/hotel/queryhotcity/1'
