eaccde84669aa0590d77bac5fdfbe56c78ac812b
zh zbin <zh zbin>
2017-06-09 09:08:06 +0800

    评论页代码提交

--- src/components/CommentPage/PicUpload/index.js
+++ src/components/CommentPage/PicUpload/index.js
@@ -9,14 +9,14 @@ export default class PicUpload extends React.Component {
   state = {
     previewVisible: false,
     previewImage: '',
-    fileList: [{
-      uid: -1,
-      name: 'xxx.png',
-      status: 'done',
-      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    }],
+    fileList: [],
   };
-
+  // {
+  //     uid: -1,
+  //     name: 'xxx.png',
+  //     status: 'done',
+  //     url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+  //   }
   handleCancel = () => this.setState({ previewVisible: false })
 
   handlePreview = (file) => {
@@ -25,9 +25,11 @@ export default class PicUpload extends React.Component {
       previewVisible: true,
     });
   }
-
+  imgRemove = (e) => {
+      console.debug(e);
+  }
   handleChange = ({ fileList }) => this.setState({ fileList })
-  
+
   render() {
     const { previewVisible, previewImage, fileList } = this.state;
     const uploadButton = (
@@ -39,29 +41,35 @@ export default class PicUpload extends React.Component {
     const props = {
       multiple: true,
     };
-        return (
-                <div className="picupload">
-                   <div className="clearfix">
-                      <Upload {...props}
-                        action="//jsonplaceholder.typicode.com/posts/"
-                        listType="picture-card"
-                        fileList={fileList}
-                        onPreview={this.handlePreview}
-                        onChange={this.handleChange}
-                      >
-                        {fileList.length >= 4 ? null : uploadButton}
-                      </Upload>
-                      <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
-                        <img alt="example" style={{ width: '100%' }} src={previewImage} />
-                      </Modal>
-                    </div>
-                    
-                    <i className="commentgift"><Icon type="gift" />上传图片 <a href="javascript:;">可抽奖</a>，赢取“香格里拉酒店免房券”！</i>
+    return (
+      <div className="picupload">
+        <div className="clearfix">
+          <Upload
+            action="biz/api/comment/upload"
+            listType="picture-card"
+            fileList={fileList}
+            onRemove={this.imgRemove.bind(this)}
+            headers={
+              {
+                'token': 'token:PC-212342343242343242334324bc36809d8-8-20170525093442-4f6496'
+              }
+            }
+            onPreview={this.handlePreview}
+            onChange={this.handleChange}
+          >
+            {fileList.length >= 4 ? null : uploadButton}
+          </Upload>
+          <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+            <img alt="example" style={{ width: '100%' }} src={previewImage} />
+          </Modal>
+        </div>
 
-                    <p className="picuploadtip">建议上传真实拍摄的酒店图片（设施、周边环境、外观等细节），最多4张。每张大小不超过2M，支持jpg,gif,png。</p>
- 
-                </div>
-             
-          )
+        <i className="commentgift"><Icon type="gift" />上传图片 <a href="javascript:;">可抽奖</a>，赢取“香格里拉酒店免房券”！</i>
+
+        <p className="picuploadtip">建议上传真实拍摄的酒店图片（设施、周边环境、外观等细节），最多4张。每张大小不超过2M，支持jpg,gif,png。</p>
+
+      </div>
+
+    )
   }
 }
